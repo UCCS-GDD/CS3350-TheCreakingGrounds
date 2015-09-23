1448e4ffba5fe28aa16ab7d01f442031189b3c3c
Conner <Kylecondo@gmail.com>
2015-09-22 20:02:36 -0600

    Created rudimentary end table, with animated drawer Began implementing player targeting reticule identification

---
+++ Unity/Assets/Scripts/CurrentPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class CurrentPlayer : Player
+    {
+        public static GameObject Instance;
+
+        public RaycastHit ReticleInfo { get; protected set; }
+
+        public override void Start()
+        {
+            base.Start();
+            Instance = gameObject;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            //get reticle target
+            RaycastHit info;
+            Physics.Raycast(gameObject.transform.position, gameObject.transform.FindChild("PlayerCamera").rotation.eulerAngles, out info, 2.0f);
+            ReticleInfo = info;
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class Player : MonoBehaviour
+    {
+        public virtual void Start()
+        {
+
+        }
+
+        public virtual void Update()
+        {
+
+        }
+    }
+}
