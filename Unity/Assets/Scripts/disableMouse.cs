8a747c0fb91d787bdf9ae671ab31c9c1f923d3d0
jmarque3 <jmarque3@uccs.edu>
2015-09-30 20:33:42 -0600

    Added Presentation Room

---
+++ Unity/Assets/Scripts/disableMouse.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class disableMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		UnityEngine.Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
