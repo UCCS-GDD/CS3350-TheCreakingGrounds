22098f6fc82517f04b53f228c9ae835b0e8e62fe
unknown <Kylecondo@gmail.com>
2015-10-07 18:54:21 -0600

    Ghost model imported Ghost pops up in middle room

---
+++ Unity/Assets/Scripts/TriggerGhost.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class TriggerGhost : MonoBehaviour
+    {
+        bool isTriggered = false;
+        float t = 0.0f;
+
+        public GameObject Ghost;
+
+        Vector3 lerpStart;
+
+        public Vector3 lerpEnd;
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject == CurrentPlayer.Instance)
+                isTriggered = true;
+        }
+
+        public void Update()
+        {
+            if (isTriggered && t <= 1)
+            {
+                Ghost.transform.position = Vector3.Lerp(lerpStart, lerpEnd, t);
+                t += Time.deltaTime;
+            }
+        }
+    }
+}
