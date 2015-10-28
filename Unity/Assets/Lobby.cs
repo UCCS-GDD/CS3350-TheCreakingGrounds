8add0f86af0279860f2f610dfaf9f9d1599c4485
jmarque3 <jmarque3@uccs.edu>
2015-10-27 18:56:09 -0600

    Multiplayer Phase 1

---
+++ Unity/Assets/Lobby.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Lobby : MonoBehaviour{
+
+    public NetworkLobbyManager networkManager;
+    public Text matchName;
+    public Text numPlayers;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Find objects
+        networkManager = GetComponent<NetworkManager>() as NetworkLobbyManager;
+        //networkManager = GetComponent<NetworkLobbyManager>();
+        matchName = GameObject.Find("MatchName").GetComponent<Text>();
+        numPlayers = GameObject.Find("NumPlayers").GetComponent<Text>();
+
+        //Setup Lobby Room
+        matchName.text = networkManager.matchName;
+        numPlayers.text = networkManager.numPlayers.ToString();
+
+        //Attempt to become a player
+        networkManager.TryToAddPlayer();
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        //Display correct number of player count
+        numPlayers.text = networkManager.numPlayers.ToString();
+    }
+}
---
+++ Unity/Assets/Scripts/Menu/SlideShow.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GUITexture))]
+public class SlideShow : MonoBehaviour
+{
+    public Texture2D[] slides = new Texture2D[1];
+    public float changeTime = 10.0f;
+    private int currentSlide = 0;
+    private float timeSinceLast = 1.0f;
+
+    void Start()
+    {
+        /*
+        GetComponent<GUITexture>().texture = slides[currentSlide];
+        GetComponent<GUITexture>().pixelInset = new Rect(-slides[currentSlide].width / 2, -slides[currentSlide].height / 2, slides[currentSlide].width, slides[currentSlide].height);
+        currentSlide++;
+         */
+    }
+
+    void Update()
+    {
+        /*
+        if (timeSinceLast > changeTime && currentSlide < slides.Length)
+        {
+            GetComponent<GUITexture>().texture = slides[currentSlide];
+            GetComponent<GUITexture>().pixelInset = new Rect(-slides[currentSlide].width / 2, -slides[currentSlide].height / 2, slides[currentSlide].width, slides[currentSlide].height);
+            timeSinceLast = 0.0f;
+            currentSlide++;
+        }
+        // comment out this section if you don't want the slide show to loop
+        // -----------------------
+        if (currentSlide == slides.Length)
+        {
+            currentSlide = 0;
+        }
+        // ------------------------
+        timeSinceLast += Time.deltaTime;
+         */
+    }
+}
