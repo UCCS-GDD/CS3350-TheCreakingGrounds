30c512dbcc178d18d59591f4f75eac2f40aee5cb
jmarque3 <jmarque3@uccs.edu>
2015-11-02 15:27:35 -0700

    Multiplayer - Phase 3 - Implement for Alpha release. Some bugs, but working.

---
+++ Unity/Assets/Lobby/Scripts/GuiLobbyManager.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class GuiLobbyManager : NetworkLobbyManager
+{
+    public LobbyCanvasControl lobbyCanvas;
+    public OfflineCanvasControl offlineCanvas;
+    public OnlineCanvasControl onlineCanvas;
+    public ConnectingCanvasControl connectingCanvas;
+    public PopupCanvasControl popupCanvas;
+
+    public string onlineStatus;
+    static public GuiLobbyManager s_Singleton;
+
+    void Start()
+    {
+        s_Singleton = this;
+        offlineCanvas.Show();
+    }
+
+    //Determine which canvas
+    void OnLevelWasLoaded()
+    {
+        if (lobbyCanvas != null) lobbyCanvas.OnLevelWasLoaded();
+        if (offlineCanvas != null) offlineCanvas.OnLevelWasLoaded();
+        if (onlineCanvas != null) onlineCanvas.OnLevelWasLoaded();
+        if (connectingCanvas != null) connectingCanvas.OnLevelWasLoaded();
+        if (popupCanvas != null) popupCanvas.OnLevelWasLoaded();
+    }
+
+    public void SetFocusToAddPlayerButton()
+    {
+        if (lobbyCanvas == null)
+            return;
+
+        lobbyCanvas.SetFocusToAddPlayerButton();
+    }
+
+    /*
+     * Server Callbacks
+    */ 
+
+    public override void OnLobbyStopHost()
+    {
+        lobbyCanvas.Hide();
+        offlineCanvas.Show();
+    }
+
+    public override bool OnLobbyServerSceneLoadedForPlayer(GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        //This hook allows you to apply state data from the lobby-player to the game-player
+        return true;
+    }
+
+    /*
+     * Client Callbacks
+    */ 
+
+    public override void OnLobbyClientConnect(NetworkConnection conn)
+    {
+        connectingCanvas.Hide();
+    }
+
+    public override void OnClientError(NetworkConnection conn, int errorCode)
+    {
+        connectingCanvas.Hide();
+        StopHost();
+
+        popupCanvas.Show("Client Error", errorCode.ToString());
+    }
+
+    public override void OnLobbyClientDisconnect(NetworkConnection conn)
+    {
+        lobbyCanvas.Hide();
+        offlineCanvas.Show();
+    }
+
+    public override void OnLobbyStartClient(NetworkClient client)
+    {
+        if (matchInfo != null)
+        {
+            connectingCanvas.Show(matchInfo.address);
+        }
+        else
+        {
+            connectingCanvas.Show(networkAddress);
+        }
+    }
+
+    public override void OnLobbyClientAddPlayerFailed()
+    {
+        popupCanvas.Show("Error", "No more players allowed.");
+    }
+
+    public override void OnLobbyClientEnter()
+    {
+        lobbyCanvas.Show();
+        onlineCanvas.Show(onlineStatus);
+    }
+
+    public override void OnLobbyClientExit()
+    {
+        lobbyCanvas.Hide();
+        onlineCanvas.Hide();
+    }
+}
