2984981ceedca2db2af8c0fe829928acc0a48248
Conner <Kylecondo@gmail.com>
2015-11-03 11:15:58 -0700

    Basic work on inventory items, perk function overhaul

--- Unity/Assets/Scripts/Perk.cs
+++ Unity/Assets/Scripts/Perk.cs
@@ -7,14 +7,37 @@ using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
-    public abstract class Perk : MonoBehaviour
+    public class Perk : MonoBehaviour
     {
         public string Name = "";
-        public string Description = "";
+        public string FlavorText = "";
         public Sprite Icon = null;
 
-        public abstract void OnAdd(Player player);
+        private List<Effect> Effects;
 
-        public abstract void OnRemove(Player player);
+        public String Description
+        {
+            get
+            {
+                return String.Format("{0}\n\n{1}", FlavorText, String.Join("\n", Effects.Select<Effect, string>(e => e.EffectDescription()).ToArray()));
+            }
+        }
+
+        void Start()
+        {
+            Effects = new List<Effect>(GetComponentsInChildren<Effect>());
+        }
+
+        public void OnAdd(Player player)
+        {
+            foreach (var effect in Effects)
+                effect.OnAdd(player);
+        }
+        
+        public void OnRemove(Player player)
+        {
+            foreach (var effect in Effects)
+                effect.OnRemove(player);
+        }
     }
 }
